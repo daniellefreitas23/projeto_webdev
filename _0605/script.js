9cc43991a01932cf68ed35e7d6b92e042479a81f
Danielle Freitas <135073652+daniellefreitas23@users.noreply.github.com>
2026-05-06 21:52:52 -0300

    Add files via upload

---
+++ _0605/script.js
@@ -0,0 +1,72 @@
+//   && = and
+//   || = or
+//   !== = diferente
+
+////////////////////////////////////////////////////////////////////////////////////////
+//             Desafio 1
+
+//function contagemRegressiva(n){
+//    while (n >= 1 ) {
+//        console.log(n);
+        //i ++;
+//        n--;
+//    }
+//}
+
+//let num = Number(prompt("Digite um número: "))
+//contagemRegressiva(num)
+
+////////////////////////////////////////////////////////////////////////////////////////
+//             Desafio 2
+
+//let nome;
+//let i;
+//let s;
+//let g;
+//let e_c;
+//do {
+//    nome = prompt("Digite seu nome: ");
+//    if(nome.length < 3){
+//        alert("Digite um nome de verdade.");
+//    };
+//} while (nome.length<3 );
+//do {
+//    i = Number(prompt("Digite sua idade: "));
+//    if(i<0 || i>150){
+//        alert("Digite sua idade de verdade.");
+//    }; 
+//} while (i<0 || i>150);
+//do {
+//    s = Number(prompt("Digite seu sálario: "));
+//    if(s<0){
+//        alert("Digite seu sálario de verdade.");
+//    };
+//} while (s<0); 
+//do {
+//    g = prompt("Digite seu gênero(f/m): ");
+//    if((g!=="f" && g!=="m") ){
+//        alert("Digite seu gênero de verdade.");
+//    };
+//} while (g!=="f" && g!=="m");
+//do {
+//    e_c = prompt("Digite seu estado civil(s, c, v, d): ");
+//    if(e_c!=="s" && e_c!=="c" && e_c!=="v" && e_c!=="d"){
+//        alert("Digite seu estado civil de verdade.");
+//    };
+//} while (e_c!=="s" && e_c!=="c" && e_c!=="v" && e_c!=="d") 
+
+////////////////////////////////////////////////////////////////////////////////////////
+//             Desafio 3
+
+//for(let i=0; i<10; i++){
+//    console.log(i);
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//             Desafio 4
+
+//n = prompt("Digite o numero da tabuada: ");
+
+//for(let i=1; i<10; i++){
+//    alert(`${i} * ${n} = ${i*n}`)
+//}
